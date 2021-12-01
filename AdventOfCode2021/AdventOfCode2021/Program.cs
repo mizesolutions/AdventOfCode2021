ea63047f730a066b178136a54813a03d33ffe40e
Brian Mize <bmize@advantageiq.com>
2021-12-01 08:57:38 -0800

    Day01 a success

--- AdventOfCode2021/AdventOfCode2021/Program.cs
+++ AdventOfCode2021/AdventOfCode2021/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using AdventOfCode2021.Days._01;
 
 namespace AdventOfCode2021
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool isInput = true;
+            var day = "";
+
+            if (args != null && args.Length > 0)
+            {
+                if (args.Length == 2 && args[0].Contains("-n"))
+                {
+                    isInput = false;
+                    day = args[1];
+                }
+                else
+                {
+                    day = args[0];
+                }
+                return RunDay(day, isInput);
+            }
+            else
+            {
+                Console.WriteLine($"No args found - \r\nUsage: \r\n\taoc21 day##   normal operation\r\n\taoc21 -n day##   to run with no input");
+                return 0;
+            }
+
+
+            static int RunDay(string day, bool flag)
+            {
+                switch (day)
+                {
+                    case "day01":
+                        _ = new Day01(day, flag);
+                        break;
+                    default:
+                        return 0;
+                }
+                return 1;
+            }
         }
     }
 }
