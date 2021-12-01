28ff699ea072a33ab8e7963bc573a7c890c74e0c
Brian Mize <bmize@advantageiq.com>
2021-12-01 12:00:36 -0800

    Created BaseDay class Setup Day 02

---
+++ AdventOfCode2021/AdventOfCode2021/Days/00/BaseDay.cs
@@ -0,0 +1,51 @@
+﻿using AdventOfCode2021.Infrastructure.Services;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace AdventOfCode2021.Days._00
+{
+    public class BaseDay
+    {
+        public RenderInput Input { get; set; }
+        public int Count { get; set; }
+
+        public BaseDay(string day, bool hasInput)
+        {
+            if (hasInput)
+            {
+                try
+                {
+                    Input = new RenderInput(day);
+                }
+                catch (FileNotFoundException fe)
+                {
+                    Console.WriteLine(fe.Message);
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public string GetCurrentMethod()
+        {
+            var st = new StackTrace();
+            var sf = st.GetFrame(1);
+
+            return $"  {sf.GetMethod().Name}";
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public string GetCurrentClass()
+        {
+            return $"--==<  {this.GetType().Name}  >==--";
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public string GetResults()
+        {
+            return $"      Result: {Count}\r\n";
+        }
+    }
+}
---
+++ AdventOfCode2021/AdventOfCode2021/Days/02/Day02.cs
@@ -0,0 +1,36 @@
+﻿using AdventOfCode2021.Days._00;
+using AdventOfCode2021.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Days._02
+{
+    public class Day02 : BaseDay
+    {
+        public Day02(string day, bool hasInput) : base(day, hasInput)
+        {
+            Console.WriteLine(GetCurrentClass());
+
+            PuzzleOne();
+            PuzzleTwo();
+        }
+
+        private void PuzzleOne()
+        {
+            Console.WriteLine(GetCurrentMethod());
+
+            Console.WriteLine(GetResults());
+        }
+
+        private void PuzzleTwo()
+        {
+            Console.WriteLine(GetCurrentMethod());
+
+            Console.WriteLine(GetResults());
+        }
+    }
+}
