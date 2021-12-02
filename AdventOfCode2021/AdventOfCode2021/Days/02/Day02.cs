2bd2ca9f45d65383ae8b019c113193162b51ad95
Brian Mize <bmize@advantageiq.com>
2021-12-02 07:00:21 -0800

    Day02 solutions

--- AdventOfCode2021/AdventOfCode2021/Days/02/Day02.cs
+++ AdventOfCode2021/AdventOfCode2021/Days/02/Day02.cs
@@ -11,26 +11,73 @@ namespace AdventOfCode2021.Days._02
 {
     public class Day02 : BaseDay
     {
+        private int Xposition { get; set; }
+        private int Yposition { get; set; }
+        private int Aim { get; set; }
+
         public Day02(string day, bool hasInput) : base(day, hasInput)
         {
-            Console.WriteLine(GetCurrentClass());
-
+            PrintCurrentClass();
+            Xposition = 0;
+            Yposition = 0;
+            Aim = 0;
             PuzzleOne();
             PuzzleTwo();
         }
 
         private void PuzzleOne()
         {
-            Console.WriteLine(GetCurrentMethod());
-
-            Console.WriteLine(GetResults());
+            PrintCurrentMethod();
+            foreach (var s in Input.Output)
+            {
+                var split = s.Split(" ");
+                switch (split[0])
+                {
+                    case "forward":
+                        Xposition += int.Parse(split[1]);
+                        break;
+                    case "down":
+                        Yposition += int.Parse(split[1]);
+                        break;
+                    case "up": 
+                        Yposition -= int.Parse(split[1]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Result = Xposition * Yposition;
+            PrintResults();
         }
 
         private void PuzzleTwo()
         {
-            Console.WriteLine(GetCurrentMethod());
-
-            Console.WriteLine(GetResults());
+            PrintCurrentMethod();
+            Result = 0;
+            Xposition = 0;
+            Yposition = 0;
+            Aim = 0;
+            foreach (var s in Input.Output)
+            {
+                var split = s.Split(" ");
+                switch (split[0])
+                {
+                    case "forward":
+                        Xposition += int.Parse(split[1]);
+                        Yposition += Aim * int.Parse(split[1]);
+                        break;
+                    case "down":
+                        Aim += int.Parse(split[1]);
+                        break;
+                    case "up":
+                        Aim -= int.Parse(split[1]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Result = Xposition * Yposition;
+            PrintResults();
         }
     }
 }
